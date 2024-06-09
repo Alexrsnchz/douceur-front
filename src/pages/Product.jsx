97d52768896c165db6f4a99074172a4fb1d0fcca
Alexrsnchz <alexrsnchz@gmail.com>
2024-06-09 18:48:14 +0200

    Intento de arreglo

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState, useContext } from 'react';
 import { Button } from '@/components/ui/button';
 import { CartContext } from '../context/CartContext';
