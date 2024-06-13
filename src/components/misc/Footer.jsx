0057b50e7c1df797aa35f15302d898b008556bed
Alexrsnchz <alexrsnchz@gmail.com>
2024-06-13 17:12:34 +0200

    Arreglos con la licencia

--- src/components/misc/Footer.jsx
+++ src/components/misc/Footer.jsx
@@ -162,10 +162,14 @@ function Footer() {
       </div>
       <div className="py-4 border-t border-gray-300 font-semibold">
         <div className="container mx-auto flex flex-col md:flex-row justify-center items-center gap-10">
-          <p className="mb-4 md:mb-0 text-center">
-            <Link to='https://creativecommons.org/licenses/by/4.0/' className="hover:text-[#d4a373]">
-              &copy; 2024 Douceur. Todos los derechos reservados.
-            </Link>
+          <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/" className="mb-4 md:mb-0 text-center">
+            <a property="dct:title" rel="cc:attributionURL" href="https://douceur-front.vercel.app/">
+              Douceur
+            </a> by <span property="cc:attributionName">Douceur</span> is licensed under 
+            <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0
+              <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt="" />
+              <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt="" />
+            </a>
           </p>
           <p className="text-center">
             <Link to="/privacidad" 
